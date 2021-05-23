096a686e4542ba49ad4d775bd530f900f5d8a3a9
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-05-23 21:33:41 +0300

    chore: prepare for version 0.2

--- src/main/java/me/ihdeveloper/spigot/devtools/mod/Main.java
+++ src/main/java/me/ihdeveloper/spigot/devtools/mod/Main.java
@@ -27,10 +27,10 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.EnumMap;
 
-@Mod(modid = "spigot-dev-tools", version = "0.1")
+@Mod(modid = "spigot-dev-tools", version = "0.2")
 public class Main {
     public static final byte protocolMajor = 0;
-    public static final byte protocolMinor = 1;
+    public static final byte protocolMinor = 2;
 
     private static Main instance;
 
